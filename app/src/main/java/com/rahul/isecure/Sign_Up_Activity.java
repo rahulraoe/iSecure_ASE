40a74edc9d6cd6dce158c609f9d4dfafe77b5373
rahulraoe <rahulraoe21@gmail.com>
2022-10-17 14:57:52 -0400

    User Registration Done.

--- app/src/main/java/com/rahul/isecure/Sign_Up_Activity.java
+++ app/src/main/java/com/rahul/isecure/Sign_Up_Activity.java
@@ -41,6 +41,126 @@ public class Sign_Up_Activity extends AppCompatActivity {
 
         getSupportActionBar().setTitle("Sign Up");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        user_name=(EditText)findViewById(R.id.user_name);
+        user_email=(EditText) findViewById(R.id.user_email);
+        user_password=(EditText) findViewById(R.id.user_password);
+        user_address=(EditText)findViewById(R.id.user_address);
+        sign_up=(RelativeLayout) findViewById(R.id.sign_up);
+
+        bottom_text=(TextView)findViewById(R.id.bottom_text);
+
+        bottom_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Sign_Up_Activity.this,Sign_In_Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ProgressDialog dialog = ProgressDialog.show(Sign_Up_Activity.this, "",
+                        "Loading. Please wait...", true);
+
+
+                if(user_name.getText().toString().isEmpty()){
+
+                    Toast.makeText(Sign_Up_Activity.this, "Please enter your name", Toast.LENGTH_SHORT).show();
+                    dialog.dismiss();
+                }
+                else if(user_email.getText().toString().isEmpty()){
+
+                    Toast.makeText(Sign_Up_Activity.this, "Please enter your email", Toast.LENGTH_SHORT).show();
+                    dialog.dismiss();
+                }
+                else if(user_password.getText().toString().isEmpty()){
+
+                    Toast.makeText(Sign_Up_Activity.this, "Please enter password", Toast.LENGTH_SHORT).show();
+                    dialog.dismiss();
+                }
+                else if(isEmailValid(user_email.getText().toString().trim())==false){
+                    Toast.makeText(Sign_Up_Activity.this, "Invalid email", Toast.LENGTH_SHORT).show();
+                    dialog.dismiss();
+                }
+                else if(user_name.getText().toString().length()<3)
+                {
+
+                    Toast.makeText(Sign_Up_Activity.this, "Name should be greater than 3 characters", Toast.LENGTH_SHORT).show();
+                    dialog.dismiss();
+                }
+                else if(user_password.getText().toString().length()<6)
+                {
+
+                    Toast.makeText(Sign_Up_Activity.this, "Password is too short. should be 6 characters.", Toast.LENGTH_SHORT).show();
+                    dialog.dismiss();
+                }
+                else if(user_password.getText().toString().length()<6)
+                {
+
+                    Toast.makeText(Sign_Up_Activity.this, "Please Enter your Address", Toast.LENGTH_SHORT).show();
+                    dialog.dismiss();
+                }
+                else
+                {
+
+                    mAuth = FirebaseAuth.getInstance();
+
+
+
+
+                    mAuth.createUserWithEmailAndPassword(user_email.getText().toString(), user_password.getText().toString())
+                            .addOnCompleteListener(Sign_Up_Activity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        FirebaseUser user = mAuth.getCurrentUser();
+                                        FirebaseFirestore db = FirebaseFirestore.getInstance();
+                                        Map<String, Object> data = new HashMap<>();
+                                        DateFormat dateFormat2 = new SimpleDateFormat("dd-MM-yyyy hh.mm aa");
+                                        final String    date = dateFormat2.format(new Date()).toString();
+
+                                        data.put("Iname", user_name.getText().toString());
+                                        data.put("Iemail", user_email.getText().toString());
+                                        data.put("Idate",date);
+                                        data.put("Icount","10000");
+                                        data.put("Igenerated","0");
+
+                                        data.put("Iuid",user.getUid());
+
+
+                                        db.collection("IUsers").document(user.getUid())
+                                                .set(data)
+                                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                    @Override
+                                                    public void onSuccess(Void aVoid) {
+                                                        dialog.dismiss();
+                                                        Intent intent=new Intent(Sign_Up_Activity.this,Home_Activity.class);
+                                                        startActivity(intent);
+                                                        finish();
+                                                    }
+                                                })
+                                                .addOnFailureListener(new OnFailureListener() {
+                                                    @Override
+                                                    public void onFailure(@NonNull Exception e) {
+                                                        Toast.makeText(Sign_Up_Activity.this, "Sign Up failed. Please try again later.", Toast.LENGTH_LONG).show();
+                                                        dialog.dismiss();
+                                                    }
+                                                });
+
+                                    }
+                                    else {
+
+                                        dialog.dismiss();
+                                        Toast.makeText(Sign_Up_Activity.this, "Authentication failed."+task.getException(),
+                                                Toast.LENGTH_SHORT).show();
+
+                                    }
+                                }
+                            });
+                }
+            }
+        });
 
 
     }
